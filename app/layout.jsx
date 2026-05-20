dd12306e3b5dfcf5bd5317be573a744561c3a532
Iva Dhooria <ivadhooria@Ivas-MacBook-Pro-2.local>
2026-05-20 13:09:10 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
       <body>
         <SpotlightBackground />
         <Navbar />
+        <ThemeToggleFloat />
         {children}
       </body>
     </html>
