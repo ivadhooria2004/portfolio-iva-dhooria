da39137ad0093cf7cf350186f6b959e7441f64ad
Iva Dhooria <ivadhooria@Ivas-MacBook-Pro-2.local>
2026-05-27 17:17:48 -0700

    Update page.jsx

--- app/honors-portfolio/relearning-authenticity/page.jsx
+++ app/honors-portfolio/relearning-authenticity/page.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import PageShell, { PageHeader } from "../../../components/PageShell";
+import PdfViewer from "../../../components/PdfViewer";
 import { artifacts } from "../artifactsData";
 
 export const metadata = { title: "Relearning Authenticity | Iva Dhooria" };
