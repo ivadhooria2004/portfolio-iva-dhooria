1acd4df2aca8580d9bad8ddbf52fc4fcc18932d4
Iva Dhooria <ivadhooria@Ivas-MacBook-Pro-2.local>
2026-05-27 19:19:21 -0700

    Update page.jsx

--- app/honors-portfolio/intentional-exploration/page.jsx
+++ app/honors-portfolio/intentional-exploration/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import PageShell, { PageHeader } from "../../../components/PageShell";
 import { artifacts } from "../artifactsData";
 
