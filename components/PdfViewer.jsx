bbf646ca194278a3d9a40fe2eefbf8bc95108886
Iva Dhooria <ivadhooria@Ivas-MacBook-Pro-2.local>
2026-05-27 17:16:13 -0700

    Update PdfViewer.jsx

---
+++ components/PdfViewer.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import { zoomPlugin } from "@react-pdf-viewer/zoom";
+import { fullScreenPlugin } from "@react-pdf-viewer/full-screen";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import "@react-pdf-viewer/zoom/lib/styles/index.css";
+import "@react-pdf-viewer/full-screen/lib/styles/index.css";
+
+export default function PdfViewer({ src, height = "600px" }) {
+  const zoomPluginInstance = zoomPlugin();
+  const fullScreenPluginInstance = fullScreenPlugin();
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({
+    sidebarTabs: [],
+    toolbarPlugin: {
+      fullScreenPlugin: fullScreenPluginInstance,
+      zoomPlugin: zoomPluginInstance,
+    },
+  });
+
+  return (
+    <div style={{ height }}>
+      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
+        <Viewer
+          fileUrl={src}
+          plugins={[
+            defaultLayoutPluginInstance,
+            zoomPluginInstance,
+            fullScreenPluginInstance,
+          ]}
+        />
+      </Worker>
+    </div>
+  );
+}
